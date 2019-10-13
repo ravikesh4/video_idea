c0c8c59d4878dc87348f22ff18001e85ab4bb3c0
ravikesh4 <ravikeshyadav4@gmail.com>
2019-10-13 23:51:15 +0530

    auth

--- app.js
+++ app.js
@@ -1,116 +1,87 @@
 const express = require('express');
+const path = require('path')
 const exphbs  = require('express-handlebars');
-const bodyParse = require('body-parser');
-const mongoose = require('mongoose');
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const session = require('express-session')
 
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// load routes
+const ideas = require('./routes/ideas')
+const users = require('./routes/users')
 
-//map global promise
+// Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
-//connect to mongoose
-
+// Connect to mongoose
 mongoose.connect('mongodb://localhost/video-dev', {
-    // useMongoClient: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useMongoClient: true
 })
-    .then(() => console.log('MongoDB connected ....'))
-    .catch(err => console.log(err));
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
-//load Idea model
-require('./models/idea');
-const Idea = mongoose.model('ideas');
+// Load Idea Model
+// require('./models/Idea');
+// const Idea = mongoose.model('ideas');
 
-//handlebars middleware
-app.engine('handlebars', exphbs());
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-//body parser middleware
-app.use(bodyParse.urlencoded({ extended: false }))
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-//parse application/json
-app.use(bodyParse.json());
+// Method override middleware
+app.use(methodOverride('_method'));
 
-//middleware
+// express session 
+app.use(session({
+    secret: 'Secret',
+    resave: true,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+  }))
 
-// app.use(function(req, res, next) {
-//     console.log(Date.now());
-//     req.name = 'ravi';
-//     next();
-// });
+app.use(flash());
 
-//index
-app.get('/',(req, res) => {
-    // console.log(req.name);
-    const title = 'Welcome1'
-    res.render('index',{
-        title: title
-    });
+// global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    res.locals.error = req.flash('error')
+    next();
+})
+
+// Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
 });
 
-//about
-
-app.get('/about', (req,res) => {
-    res.render('about');
+// About Route
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
-//ideas index 
-app.get('/ideas', (req, res) => {
-    Idea.find({})
-    .sort({date: 'desc'})
-    .then(ideas => {
-        res.render('ideas/index', {
-            ideas:ideas
-        });
-    });
-    // res.render('ideas/index');
-});
 
-//Add Indeas
 
-app.get('/ideas/add', (req,res) => {
-    res.render('ideas/add');
-});
-
-//process form
-app.post('/ideas',(req, res) =>{
-    // console.log(req.body)
-    // res.send('ok');
-
-    let errors = [];
-
-    if(!req.body.title){
-        errors.push({text: 'Please add a title'});
-    }
-    if(!req.body.details){
-        errors.push({text: 'Please add a details'});
-    }
-    if(errors.length > 0) {
-        res.render('ideas/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-    } else {
-        const newUser = {
-            title: req.body.title,
-            details: req.body.details,
-            // user: req.user.id
-        }
-        new Idea(newUser)
-        .save()
-        .then(idea => {
-            res.redirect('/ideas');
-        })
-}
-});
-
+//use routes
+app.use('/ideas', ideas)
+app.use('/users', users)
 
 const port = 5000;
 
-app.listen(port , () => {
-    console.log(`server started on ${port}`);
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
 });
